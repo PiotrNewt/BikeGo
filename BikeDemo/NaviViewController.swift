04f3bbe623b3f23e5a579ff2cd56ba5c1ff7b1d3
Mclarenyang <yang.jian1@foxmail.com>
2018-03-21 18:22:19 +0800

    update touchPin

--- BikeDemo/NaviViewController.swift
+++ BikeDemo/NaviViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import Hero
 
-class NaviViewController: UIViewController, MAMapViewDelegate, AMapLocationManagerDelegate {
+class NaviViewController: UIViewController, AMapLocationManagerDelegate{
 
+    let search = AMapSearchAPI()
+    let pointAnnotation = MAPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,13 +29,26 @@ class NaviViewController: UIViewController, MAMapViewDelegate, AMapLocationManag
         mapView.isRotateEnabled = true
         mapView.showsCompass = false
         mapView.zoomLevel = 17
+        mapView.touchPOIEnabled = true
         self.view.addSubview(mapView)
         
         //定位蓝点
         let r = MAUserLocationRepresentation()
+        //朝向
+        r.showsHeadingIndicator = true
         r.showsAccuracyRing = true
         mapView.update(r)
-
+        
+        
+        //搜索
+/*
+        search?.delegate = self
+        let request = AMapPOIKeywordsSearchRequest()
+        request.keywords = "肯德基"
+        request.requireExtension = true
+        
+        request.requireSubPOIs = true
+*/
         // Do any additional setup after loading the view.
     }
 
@@ -40,7 +56,6 @@ class NaviViewController: UIViewController, MAMapViewDelegate, AMapLocationManag
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -53,3 +68,57 @@ class NaviViewController: UIViewController, MAMapViewDelegate, AMapLocationManag
     */
 
 }
+
+
+extension NaviViewController:AMapSearchDelegate, MAMapViewDelegate {
+    
+    func onPOISearchDone(_ request: AMapPOISearchBaseRequest!, response: AMapPOISearchResponse!) {
+        if response.count == 0 {
+            return
+        }
+        //解析搜索返回值
+    }
+    
+    func mapView(_ mapView: MAMapView!, didTouchPois pois: [Any]!) {
+        //显示标记
+        
+        let touchPoi = pois[0] as! MATouchPoi
+        print(touchPoi.name)
+        print(touchPoi.coordinate)
+        print(touchPoi.uid)
+        
+        
+        pointAnnotation.coordinate = touchPoi.coordinate
+        pointAnnotation.title = touchPoi.name
+        mapView.addAnnotation(pointAnnotation)
+        mapView.setCenter(touchPoi.coordinate, animated: true)
+        
+        }
+    
+    //标记方法
+    func mapView(_ mapView: MAMapView!, viewFor annotation: MAAnnotation!) -> MAAnnotationView! {
+        
+        if annotation.isKind(of: MAPointAnnotation.self) {
+            let pointReuseIndetifier = "pointReuseIndetifier"
+            var annotationView: MAAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: pointReuseIndetifier)
+            
+            if annotationView == nil {
+                annotationView = MAAnnotationView(annotation: annotation, reuseIdentifier: pointReuseIndetifier)
+            }
+            
+            annotationView!.canShowCallout = true
+            annotationView!.isDraggable = true
+            annotationView!.rightCalloutAccessoryView = UIButton(type: UIButtonType.detailDisclosure)
+            
+            annotationView!.image = #imageLiteral(resourceName: "BluePin")
+            //设置中心点偏移，使得标注底部中间点成为经纬度对应点
+            annotationView!.centerOffset = CGPoint(x:2.5, y:-24);
+            
+            return annotationView!
+        }
+        
+        return nil
+    }
+}
+
+
